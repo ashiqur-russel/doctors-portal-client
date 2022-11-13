7612c6b2e08c0797f3d3344be270954d4f73be88
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-13 14:50:25 +0100

    testimonial section design

--- src/Pages/Home/Testimonials/Testimonial.js
+++ src/Pages/Home/Testimonials/Testimonial.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const Testimonial = () => {
-  return <div></div>;
+const Testimonial = ({ review }) => {
+  const { name, img, review: userReview, location } = review;
+  console.log(review);
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="flex items-center">
+          <div className="avatar mr-6">
+            <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img src={img} alt="" className="" />
+            </div>
+          </div>
+          <div>
+            <h5 className="text-lg">{name}</h5>
+            <p>{location}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
--- src/Pages/Home/Testimonials/Testimonials.js
+++ src/Pages/Home/Testimonials/Testimonials.js
@@ -2,7 +2,7 @@ import React from "react";
 import quote from "../../../assets/icons/quote.svg";
 import people1 from "../../../assets/images/people1.png";
 import people2 from "../../../assets/images/people2.png";
-import people3 from "../../../assets/images/people2.png";
+import people3 from "../../../assets/images/people3.png";
 import Testimonial from "./Testimonial";
 
 const Testimonials = () => {
@@ -11,28 +11,28 @@ const Testimonials = () => {
       _id: 1,
       name: "Ashiqur Russel",
       review: "",
-      loacation: "Berlin",
+      location: "Berlin",
       img: people1,
     },
     {
-      _id: 1,
+      _id: 2,
       name: "Biplob Bipu",
       review: "",
-      loacation: "Chemnitz",
+      location: "Chemnitz",
       img: people2,
     },
     {
-      _id: 1,
+      _id: 3,
       name: "Saad Abdullah",
       review: "",
-      loacation: "Nürnberg",
+      location: "Nürnberg",
       img: people3,
     },
   ];
   return (
     <section className="my-16">
       <div className="flex justify-between">
-        <div>
+        <div className="">
           <h4 className="text-xl text-primary font-bold">Testimonial</h4>
           <h2 className="text-4´3xl">What Our Patients Says</h2>
         </div>
@@ -41,7 +41,7 @@ const Testimonials = () => {
           <img src={quote} alt="" className="w-24 lg:w-48" />
         </figure>
       </div>
-      <div>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {reviews.map((review) => (
           <Testimonial key={review._id} review={review}></Testimonial>
         ))}
