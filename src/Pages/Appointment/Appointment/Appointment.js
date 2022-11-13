7f4cc7a0c69591897f13ecfbba8635891be99bb1
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-13 22:54:18 +0100

    Appointment Options and available slots with conditional rendering

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -1,15 +1,9 @@
 import React, { useState } from "react";
 import AppointmentBanner from "../AppointmentBanner/AppointmentBanner";
 import AvailableAppointments from "../AvailableAppointments/AvailableAppointments";
-import { format } from "date-fns";
 
 const Appointment = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const footer = selectedDate ? (
-    <p>You selected {format(selectedDate, "PPP")}.</p>
-  ) : (
-    <p>Please pick a day.</p>
-  );
 
   return (
     <div>
