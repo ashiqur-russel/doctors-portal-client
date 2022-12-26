4cb29bd83100b8fe7e779d11bb657ca4c15f6b1e
ashiqurras <ashiqurrussel2@gmail.com>
2022-12-27 00:58:38 +0100

    error message display for card insertion error

--- src/Pages/Dashboard/Payment/CheckoutForm.js
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -5,7 +5,9 @@ import {
   useElements,
   useStripe,
 } from "@stripe/react-stripe-js";
+import { useState } from "react";
 const CheckoutForm = () => {
+  const [cardError, setCardError] = useState("");
   const stripe = useStripe();
   const elements = useElements();
   const handleSubmit = async (event) => {
@@ -35,36 +37,41 @@ const CheckoutForm = () => {
 
     if (error) {
       console.log("[error]", error);
+      setCardError(error.message);
     } else {
       console.log("[PaymentMethod]", paymentMethod);
+      setCardError("");
     }
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button
-        className="btn btn-sm mt-4 btn-primary"
-        type="submit"
-        disabled={!stripe}
-      >
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <button
+          className="btn btn-sm mt-4 btn-primary"
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+      <p className="text-red-300">{cardError}</p>
+    </>
   );
 };
 
